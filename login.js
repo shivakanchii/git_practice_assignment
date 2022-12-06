0b94845710e4c5cd9efa41579125ffcbf1ef320e
shivakanchi <shivanani157@gmail.com>
2022-12-06 17:46:27 +0530

    login_reg

---
+++ login.js
@@ -0,0 +1,23 @@
+let num=7;
+let count=0;
+
+for(let i=2;i<=num;i++){
+  if(num%1==0){
+    count++
+  }
+}
+if(count==0){
+  console.log("Yes")
+}else{
+  console.log("No")
+}
+}
+
+function(palindrome)
+
+let str="naman"
+let rev=""
+
+for(let i=str.length;i>=0;i--){
+  rev+=str[i]
+}console.log(rev==str)
